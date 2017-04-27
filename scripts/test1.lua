18b71de3db5d6eb8ac136c3ee95c06c1a6d599a7
Luca Magnabosco <luca@intermodalics.eu>
2017-04-27 12:07:19 +0200

    add comments to test1.lua

--- scripts/test1.lua
+++ scripts/test1.lua
@@ -1,8 +1,13 @@
 require "rfsm"
 
+-- Load the desired rfsm model.
 model=rfsm.load("coordination/parallel_transitions_1.lua")
+-- Initialize the rfsm model.
 fsm=rfsm.init(model)
 
+-- Use the rfsm.
+-- Run the rfsm to let it handle the events and the transitions.
 rfsm.run(fsm)
+-- Send new events to the rfsm.
 rfsm.send_events(fsm,"e13","e12_e34")
 rfsm.run(fsm)
